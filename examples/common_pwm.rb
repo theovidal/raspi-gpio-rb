fdbd78a95c8a3c3227a7dfcee8c6d80d6f63ad63
Sergey Gernyak <sergeg1990@gmail.com>
2022-08-02 12:39:18 +0300

    Add output into the example script

--- examples/common_pwm.rb
+++ examples/common_pwm.rb
@@ -1,12 +1,16 @@
 require_relative '../lib/raspi-pwm.rb'
 
+puts 'Initializing PWM #0 channel...'
 pwm = RaspiPWM.new(channel: 0)
 # to give for the system some time for creating
 # needed files and setting appropriate permissions
 sleep(0.5)
+puts 'Setting PWM parameters'
 pwm.frequency = 2
 pwm.duty_cycle = 50
 pwm.enabled = true
+puts 'PWM was enabled! Letting it to run for the next 10 seconds...'
 sleep(10)
-pwm.enabled = false
+puts 'Done'
 pwm.cleanup
+puts 'Everything was cleaned up! Good bye...'
